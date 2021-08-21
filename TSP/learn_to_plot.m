2a607a25517463a2059f94a15a25eb1f57332918
sisifo3 <49049237+sisifo3@users.noreply.github.com>
2021-08-21 17:55:22 -0500

    Add files via upload

---
+++ TSP/learn_to_plot.m
@@ -0,0 +1,16 @@
+function[anser] = learn_to_plot(x,a,b)
+%x = [1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18];
+%a = [1,2,2,4,5,5,6,8,10,10,11,12,12,16,17,17,18,18];
+%b = [1,1,2,3,3,4,6,6,6,9,9,9,9,10,11,12,17,18,];
+
+a1 = plot(x,a);
+xlabel('Generación')
+ylabel('Aptitud por Geneación')
+M1 = "David";
+M2 = "PMx";
+hold on
+a2=plot(x,b);
+legend([a1,a2], [M1, M2]);
+hold off
+anser = 1;
+end
---
+++ TSP/mainFunction.m
@@ -0,0 +1,91 @@
+function[anser] = mainFunction()
+close all;
+clc;
+
+%=======1 for PMx and other for David ===== 
+crossover_tec = 0;
+%=================end=====================
+%============help==========
+dista_pid = csvread('Tdistance_2.csv');
+%=============end=========
+number_of_solution = 100;
+sol = number_of_solution;
+
+celda_num = zeros(sol,3);
+celda_num = num2cell(celda_num);
+
+cell_num = zeros(sol*2,3);
+cell_num = num2cell(cell_num);
+
+mid_sol = sol/2;
+generations = 50;
+
+it_gen = zeros(1,generations);
+sum_aptd = zeros(1,generations);
+
+best_solution_per_generation = zeros(generations,3);
+best_solution_per_generation = num2cell(best_solution_per_generation);
+
+bes_ind_pmx = zeros(1,generations);
+wor_ind_pmx = zeros(1,generations);
+
+
+
+
+for i = 1:sol
+    arr_temp =  randperm(18,18);
+    celda_num{i,1} = arr_temp;
+    cell_num{i,1} = arr_temp;
+end
+
+[celda_num] = make_dist_apt(celda_num,dista_pid);
+
+for iter = 1:generations
+    for rip = 1:mid_sol
+        [pred1,pred2] = sel_pad_best(celda_num);
+        
+        if crossover_tec == 1
+            [desc1,desc2] = crossover_PMx_18(pred1,pred2);
+        else
+            [desc1,desc2] = order_crossover_Davids(pred1,pred2);
+        end
+        
+        cell_num{rip+sol,1} = desc1;
+        cell_num{rip+mid_sol*3,1} = desc2;
+    end
+    
+    [cell_num] = make_dist_apt(cell_num,dista_pid);
+    [cell_num] = biology_competition(cell_num,dista_pid);
+    
+    b = mod(iter,10);
+    if b == 0
+        disp('heuristic')
+        [cell_num] = heuristic_mutation(cell_num,dista_pid);
+    end
+         
+    [sum_apt] = apt_for_gene(cell_num);
+    sum_aptd(iter) =  sum_apt;
+    it_gen(iter) =  iter;
+    
+    best_solution_per_generation(iter,:) = cell_num(1,:);
+    bes_ind_pmx(iter) = cell_num{1,3}; 
+    wor_ind_pmx(iter) = cell_num{100,3};
+    
+    
+    disp(iter)
+    celda_num = cell_num;
+end
+%[apt_david,bes_ind,wos_ind] = cangrejo_david();
+
+
+
+%disp(best_solution_per_generation)
+%[wx] = learn_to_plot(it_gen,apt_david,sum_aptd);
+[wz] = learn_to_plot_bestind(it_gen,bes_ind_pmx);
+
+
+anser = cell_num;
+
+
+
+end
